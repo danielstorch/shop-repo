88b9ce9bff88114d610b4c212e08ed5f99428532
Daniel Storch <danielstorch4@hotmail.com>
2013-10-31 14:58:08 +0100

    Bestellverwaltung angelegt

---
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellung.java
@@ -0,0 +1,14 @@
+package de.shop.bestellverwaltung.domain;
+
+import java.io.Serializable;
+import java.net.URI;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import de.shop.kundenverwaltung.domain.AbstractKunde;
+
+@XmlRootElement
+public class Bestellung {
+
+}
