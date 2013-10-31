0d7f7277ac5e1474e1bc5d49b5803715f72880f9
Mihai Sava <saio1011@hs-karlsruhe.de>
2013-10-31 17:44:30 +0100

    Adresse, FirmenKunde, Hobby, PrivatKunde erweitern

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Firmenkunde.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Firmenkunde.java
@@ -1,5 +1,8 @@
 package de.shop.kundenverwaltung.domain;
 
-public class Firmenkunde {
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
+public class Firmenkunde extends AbstractKunde {
+	private static final long serialVersionUID = 6258156986876418100L;
 }
