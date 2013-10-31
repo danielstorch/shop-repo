bc6510c8c49dc8fa0e2396fa52eaec972112e5a4
Mihai Sava <saio1011@hs-karlsruhe.de>
2013-10-31 17:43:38 +0100

    Bestellung erweitern

--- shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellung.java
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellung.java
@@ -10,5 +10,14 @@ import de.shop.kundenverwaltung.domain.AbstractKunde;
 
 @XmlRootElement
 public class Bestellung {
+	private static final long serialVersionUID = 1618359234119003714L;
+	
+	private Long id;
+	private boolean ausgeliefert;
+	
+	@XmlTransient
+	private AbstractKunde kunde;
+	
+	private URI kundeUri;
 
 }
