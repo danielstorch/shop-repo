3462f813f3b03dabd4d0eaae9e158997e4a4d231
Daniel Storch <danielstorch4@hotmail.com>
2013-10-31 14:41:43 +0100

    AbstractKunde angelegt

---
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/AbstractKunde.java
@@ -0,0 +1,19 @@
+package de.shop.kundenverwaltung.domain;
+
+import java.io.Serializable;
+import java.net.URI;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.codehaus.jackson.annotate.JsonSubTypes;
+import org.codehaus.jackson.annotate.JsonSubTypes.Type;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
+
+import de.shop.bestellverwaltung.domain.Bestellung;
+
+public abstract class AbstractKunde {
+
+}
